8aaf41ad943f9b56598921465b325149e99356cf
Elvin_1492 <elvin.arzumanoglu@gmail.com>
2022-06-28 18:16:32 +0400

    adding baseapp service

--- BaseAppService.cs
+++ BaseAppService.cs
@@ -17,56 +17,6 @@ namespace Service.Base
         {
             this._httpContext = httpContext;
         }
-        public string PrepareFileForUpload(UploadFileDto input)
-        {
-            var folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"TempFiles");
-
-            if (!Directory.Exists(folder))
-            {
-                Directory.CreateDirectory(folder);
-            }
-
-            var id = Guid.NewGuid().ToString().Replace("-", "").ToUpper();
-            var fullPath = $"{folder}\\{(string.IsNullOrWhiteSpace(input.Name) ? "" : Path.GetFileNameWithoutExtension(input.Name) + "_")}{id}{input.FileExtension}";
-
-            byte[] bytes = Convert.FromBase64String(input.Content);
-
-            File.WriteAllBytes(fullPath, bytes);
-
-            return fullPath;
-        }
-
-        public static string PrepareFileForAttachmentsUpload(UploadFileDto input, IConfiguration configuration, bool isLocalFolder = true, string folder = "")
-        {
-            if (isLocalFolder)
-            {
-                folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "@Templates");
-            }
-
-            if (!Directory.Exists(folder.ToString()))
-            {
-                Directory.CreateDirectory(folder.ToString());
-            }
-            var id = Guid.NewGuid().ToString().Replace("-", "").ToUpper();
-
-            var fullPath = $"{folder}\\{(string.IsNullOrWhiteSpace(input.Name) ? "" : Path.GetFileNameWithoutExtension(input.Name) + "_")}{id}{input.FileExtension}";
-
-            byte[] bytes = Convert.FromBase64String(input.Content);
-
-
-            File.WriteAllBytes(fullPath, bytes);
-
-            var actualType = MimeTypes.GetContentType(fullPath);
-
-            if (actualType.EndsWith("jpg") || actualType.EndsWith("jpeg") || actualType.EndsWith("png") || actualType.EndsWith("pdf"))
-            {
-                return $"Attachments/{Path.GetFileNameWithoutExtension(input.Name)}_{id}{input.FileExtension}";
-            }
-            else
-            {
-                throw new Exception("File is not Valid");
-            }
-        }
 
         public Guid? CurrentUserId
         {
